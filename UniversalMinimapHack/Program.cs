820f0788975411678287c35810e6a8e76beb0ddf
TheSaltyWaffle <thesaltywaffle@users.noreply.github.com>
2014-11-24 18:53:25 +0100

    increased accuracy + fallback ping + slider

--- UniversalMinimapHack/Program.cs
+++ UniversalMinimapHack/Program.cs
@@ -13,14 +13,16 @@ using SharpDX;
 
 namespace UniversalMinimapHack
 {
-    class Program
+    internal class Program
     {
         private static Program _instance;
         private string _version;
         private readonly IList<Position> _positions = new List<Position>();
         private MenuItem _slider;
+        private MenuItem SsFallbackPing;
         public MenuItem SsTimerEnabler { get; set; }
-        static void Main(string[] args)
+
+        private static void Main(string[] args)
         {
             GetInstance();
         }
@@ -40,7 +42,7 @@ namespace UniversalMinimapHack
             CustomEvents.Game.OnGameLoad += Game_OnGameLoad;
         }
 
-        void Game_OnGameLoad(EventArgs args)
+        private void Game_OnGameLoad(EventArgs args)
         {
             Menu menu = new Menu("Universal MinimapHack", "UniversalMinimapHack", true);
             _slider = new MenuItem("scale", "Icon Scale % (F5 to Reload)").SetValue(new Slider(20));
@@ -49,10 +51,19 @@ namespace UniversalMinimapHack
                 new MenuItem("enableSS", "Enable SS Timer").SetValue(true);
             SsTimerSize = new MenuItem("sizeSS", "SS Text Size (F5 to Reload)").SetValue(new Slider(15));
             SsTimerOffset = new MenuItem("offsetSS", "SS Text Height").SetValue(new Slider(15, -50, +50));
+            SsTimerMin = new MenuItem("minSS", "Show after X seconds").SetValue(new Slider(30, 1, 180));
+            SsTimerMinPing = new MenuItem("minPingSS", "Ping after X seconds").SetValue(new Slider(30, 5, 180));
+            SsFallbackPing = new MenuItem("fallbackSS", "Fallback ping (local)").SetValue(false);
+            menu.AddItem(new MenuItem("", "[Customize]"));
             menu.AddItem(_slider);
             menu.AddItem(IconOpacity);
             Menu ssMenu = new Menu("SS Timer", "ssTimer");
             ssMenu.AddItem(SsTimerEnabler);
+            ssMenu.AddItem(new MenuItem("1", "[Extra]"));
+            ssMenu.AddItem(SsTimerMin);
+            ssMenu.AddItem(SsFallbackPing);
+            ssMenu.AddItem(SsTimerMinPing);
+            ssMenu.AddItem(new MenuItem("2", "[Customize]"));
             ssMenu.AddItem(SsTimerSize);
             ssMenu.AddItem(SsTimerOffset);
             menu.AddSubMenu(ssMenu);
@@ -106,36 +117,55 @@ namespace UniversalMinimapHack
                 {
                     pos.Text.OnEndScene();
                 }
-
             }
         }
 
-        void Game_OnGameUpdate(EventArgs args)
+        private void Game_OnGameUpdate(EventArgs args)
         {
-
             foreach (Position pos in _positions)
             {
-                if (pos.Hero.IsVisible)
+                if (pos.Hero.ServerPosition != pos.LastLocation)
                 {
+                    pos.LastLocation = pos.Hero.ServerPosition;
                     pos.LastSeen = Game.ClockTime;
                 }
+
+                if (pos.Hero.IsVisible && !pos.Hero.IsDead)
+                {
+                    pos.Pinged = false;
+                }
+
+
+                if (pos.LastSeen != 0f && SsFallbackPing.GetValue<bool>() && !pos.Hero.IsDead)
+                {
+                    if (Game.ClockTime - pos.LastSeen >= SsTimerMinPing.GetValue<Slider>().Value && !pos.Pinged)
+                    {
+                        Packet.S2C.Ping.Encoded(new Packet.S2C.Ping.Struct(pos.LastLocation.X, pos.LastLocation.Y, pos.Hero.NetworkId,
+                            ObjectManager.Player.NetworkId, Packet.PingType.EnemyMissing)).Process();
+                        pos.Pinged = true;
+                    } 
+                }
+                
             }
         }
 
-        float GetScale()
+        private float GetScale()
         {
-            return _slider.GetValue<Slider>().Value / 100f;
+            return _slider.GetValue<Slider>().Value/100f;
         }
 
-        void LoadImages()
+        private void LoadImages()
         {
-            foreach (Obj_AI_Hero hero in ObjectManager.Get<Obj_AI_Hero>().Where(hero => hero != null && hero.Team != ObjectManager.Player.Team && hero.IsValid))
+            foreach (
+                Obj_AI_Hero hero in
+                    ObjectManager.Get<Obj_AI_Hero>()
+                        .Where(hero => hero != null && hero.Team != ObjectManager.Player.Team && hero.IsValid))
             {
                 LoadImage(hero);
             }
         }
 
-        void LoadImage(Obj_AI_Hero hero)
+        private void LoadImage(Obj_AI_Hero hero)
         {
             Bitmap bmp = null;
             if (File.Exists(GetImageCached(hero.ChampionName)))
@@ -166,15 +196,17 @@ namespace UniversalMinimapHack
 
             if (bmp != null)
             {
-                Position pos = new Position(hero, ChangeOpacity(bmp, IconOpacity.GetValue<Slider>().Value/100f), GetScale());
+                Position pos = new Position(hero, ChangeOpacity(bmp, IconOpacity.GetValue<Slider>().Value/100f),
+                    GetScale());
                 _positions.Add(pos);
             }
-
         }
 
         private Bitmap DownloadImage(string champName)
         {
-            WebRequest request = WebRequest.Create("http://ddragon.leagueoflegends.com/cdn/" + _version + "/img/champion/" + champName + ".png");
+            WebRequest request =
+                WebRequest.Create("http://ddragon.leagueoflegends.com/cdn/" + _version + "/img/champion/" + champName +
+                                  ".png");
             Stream responseStream;
             using (WebResponse response = request.GetResponse())
             using (responseStream = response.GetResponseStream())
@@ -184,7 +216,8 @@ namespace UniversalMinimapHack
         public Bitmap CreateFinalImage(Bitmap srcBitmap, int circleUpperLeftX, int circleUpperLeftY, int circleDiameter)
         {
             Bitmap finalImage = new Bitmap(circleDiameter, circleDiameter);
-            System.Drawing.Rectangle cropRect = new System.Drawing.Rectangle(circleUpperLeftX, circleUpperLeftY, circleDiameter, circleDiameter);
+            System.Drawing.Rectangle cropRect = new System.Drawing.Rectangle(circleUpperLeftX, circleUpperLeftY,
+                circleDiameter, circleDiameter);
 
             using (Bitmap sourceImage = srcBitmap)
             using (Bitmap croppedImage = sourceImage.Clone(cropRect, sourceImage.PixelFormat))
@@ -192,7 +225,7 @@ namespace UniversalMinimapHack
             using (Graphics g = Graphics.FromImage(finalImage))
             {
                 g.FillEllipse(tb, 0, 0, circleDiameter, circleDiameter);
-                Pen p = new Pen(System.Drawing.Color.DarkRed, 10) { Alignment = PenAlignment.Inset };
+                Pen p = new Pen(System.Drawing.Color.DarkRed, 10) {Alignment = PenAlignment.Inset};
                 g.DrawEllipse(p, 0, 0, circleDiameter, circleDiameter);
             }
             return finalImage;
@@ -202,24 +235,27 @@ namespace UniversalMinimapHack
         {
             Bitmap bmp = new Bitmap(img.Width, img.Height); // Determining Width and Height of Source Image
             Graphics graphics = Graphics.FromImage(bmp);
-            ColorMatrix colormatrix = new ColorMatrix { Matrix33 = opacityvalue };
+            ColorMatrix colormatrix = new ColorMatrix {Matrix33 = opacityvalue};
             ImageAttributes imgAttribute = new ImageAttributes();
             imgAttribute.SetColorMatrix(colormatrix, ColorMatrixFlag.Default, ColorAdjustType.Bitmap);
-            graphics.DrawImage(img, new System.Drawing.Rectangle(0, 0, bmp.Width, bmp.Height), 0, 0, img.Width, img.Height, GraphicsUnit.Pixel, imgAttribute);
-            graphics.Dispose();   // Releasing all resource used by graphics
+            graphics.DrawImage(img, new System.Drawing.Rectangle(0, 0, bmp.Width, bmp.Height), 0, 0, img.Width,
+                img.Height, GraphicsUnit.Pixel, imgAttribute);
+            graphics.Dispose(); // Releasing all resource used by graphics
             img.Dispose();
             return bmp;
         }
 
         private void Print(string msg)
         {
-            Game.PrintChat("<font color='#ff3232'>Universal</font><font color='#BABABA'>MinimapHack:</font> <font color='#FFFFFF'>" + msg + "</font>");
+            Game.PrintChat(
+                "<font color='#ff3232'>Universal</font><font color='#BABABA'>MinimapHack:</font> <font color='#FFFFFF'>" +
+                msg + "</font>");
         }
 
         public string GameVersion()
         {
             String json = new WebClient().DownloadString("http://ddragon.leagueoflegends.com/realms/euw.json");
-            return (string)new JavaScriptSerializer().Deserialize<Dictionary<String, Object>>(json)["v"];
+            return (string) new JavaScriptSerializer().Deserialize<Dictionary<String, Object>>(json)["v"];
         }
 
         public string GetImageCached(string champName)
@@ -239,12 +275,15 @@ namespace UniversalMinimapHack
         }
 
 
-
         public MenuItem SsTimerSize { get; set; }
 
         public MenuItem SsTimerOffset { get; set; }
 
         public MenuItem IconOpacity { get; set; }
+
+        public MenuItem SsTimerMin { get; set; }
+
+        public MenuItem SsTimerMinPing { get; set; }
     }
 
 
@@ -256,6 +295,9 @@ namespace UniversalMinimapHack
         public Render.Text Text { get; set; }
         public Obj_AI_Hero Hero { get; set; }
         public float LastSeen { get; set; }
+        public Vector3 LastLocation { get; set; }
+        public Vector3 LastLocationVisible { get; set; }
+        public bool Pinged { get; set; }
 
         public Position(Obj_AI_Hero hero, Bitmap bmp, float scale)
         {
@@ -267,17 +309,22 @@ namespace UniversalMinimapHack
             Image.PositionUpdate = delegate
             {
                 Vector2 v2 = Drawing.WorldToMinimap(hero.ServerPosition);
-                v2.X -= Image.Width / 2f;
-                v2.Y -= Image.Height / 2f;
+                v2.X -= Image.Width/2f;
+                v2.Y -= Image.Height/2f;
                 return v2;
             };
             Image.Add(_layer);
-            LastSeen = Game.ClockTime;
+            LastSeen = 0;
+            LastLocation = hero.ServerPosition;
+            LastLocationVisible = hero.ServerPosition;
 
-            Text = new Render.Text(0, 0, "", Program.GetInstance().SsTimerSize.GetValue<Slider>().Value, SharpDX.Color.White)
+            Text = new Render.Text(0, 0, "", Program.GetInstance().SsTimerSize.GetValue<Slider>().Value,
+                SharpDX.Color.White)
             {
                 VisibleCondition =
-                    sender => !hero.IsVisible && Program.GetInstance().SsTimerEnabler.GetValue<bool>() && LastSeen > 20f,
+                    sender =>
+                        !hero.IsVisible && Program.GetInstance().SsTimerEnabler.GetValue<bool>() && LastSeen > 20f &&
+                        Program.GetInstance().SsTimerMin.GetValue<Slider>().Value <= Game.ClockTime - LastSeen,
                 PositionUpdate = delegate
                 {
                     Vector2 v2 = Drawing.WorldToMinimap(hero.ServerPosition);
@@ -302,6 +349,5 @@ namespace UniversalMinimapHack
             }
             return t.Minutes + ":0" + t.Seconds;
         }
-
     }
 }
