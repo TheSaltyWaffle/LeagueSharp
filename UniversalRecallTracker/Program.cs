7b76ebe7f2591f2afb9c85d63f7b4aa628de5a88
TheSaltyWaffle <thesaltywaffle@users.noreply.github.com>
2015-01-19 13:17:34 +0100

    RecallTracker: configurable scale

--- UniversalRecallTracker/Program.cs
+++ UniversalRecallTracker/Program.cs
@@ -18,7 +18,7 @@ namespace UniversalRecallTracker
         private MenuItem _x;
         private MenuItem _textSize;
         private MenuItem _chatWarning;
-        private MenuItem _showOnStart;
+        private MenuItem _barScale;
 
         public int X
         {
@@ -35,14 +35,14 @@ namespace UniversalRecallTracker
             get { return _textSize.GetValue<Slider>().Value; }
         }
 
+        public float BarScale
+        {
+            get { return _barScale.GetValue<Slider>().Value / 100f; }
+        }
+
         public bool ChatWarning
         {
             get { return _chatWarning.GetValue<bool>(); }
-        }
-
-        public bool ShowOnStart
-        {
-            get { return _showOnStart.GetValue<bool>(); }
         }
 
         public static void Main(string[] args)
@@ -73,19 +73,22 @@ namespace UniversalRecallTracker
                     new Slider(
                         (int) ((Drawing.Direct3DDevice.Viewport.Width - Properties.Resources.RecallBar.Width) / 2f), 0,
                         Drawing.Direct3DDevice.Viewport.Width));
+            _x.ValueChanged += _x_ValueChanged;
             _y =
                 new MenuItem("y", "Y").SetValue(
                     new Slider(
                         (int) (Drawing.Direct3DDevice.Viewport.Height * 3f / 4f), 0,
                         Drawing.Direct3DDevice.Viewport.Height));
+            _y.ValueChanged += _x_ValueChanged;
             _textSize = new MenuItem("textSize", "Text Size (F5 Reload)").SetValue(new Slider(15, 5, 50));
             _chatWarning = new MenuItem("chatWarning", "Chat Notification").SetValue(false);
-            _showOnStart = new MenuItem("showOnStart", "Show on Start").SetValue(true);
+            _barScale = new MenuItem("barScale", "Bar Scale %").SetValue(new Slider(100, 0, 200));
+            _barScale.ValueChanged += _barScale_ValueChanged;
 
-            menu.AddItem(_showOnStart);
             menu.AddItem(_x);
             menu.AddItem(_y);
             menu.AddItem(_textSize);
+            menu.AddItem(_barScale);
             menu.AddItem(_chatWarning);
             menu.AddToMainMenu();
 
@@ -98,6 +101,22 @@ namespace UniversalRecallTracker
             }
             //_recallInfo[ObjectManager.Player] = new RecallInfo(ObjectManager.Player,i);
             Print("Loaded!");
+        }
+
+       private void _barScale_ValueChanged(object sender, OnValueChangeEventArgs e)
+        {
+            foreach (RecallInfo info in _recallInfo.Values)
+            {
+                info.Scale(BarScale);
+            }
+        }
+
+        private void _x_ValueChanged(object sender, OnValueChangeEventArgs e)
+        {
+            foreach (RecallInfo info in _recallInfo.Values)
+            {
+                info.Reset();
+            }
         }
 
         public void Print(string msg, bool timer = false)
@@ -144,15 +163,16 @@ namespace UniversalRecallTracker
 
         private readonly Render.Sprite _sprite;
         private readonly Render.Text _countdownText;
+        private int lastChange;
 
         public RecallInfo(Obj_AI_Hero hero, int index)
         {
-            _active = Program.Instance().ShowOnStart;
             _hero = hero;
             _index = index;
             _sprite = new Render.Sprite(Properties.Resources.RecallBar, new Vector2(0, 0))
             {
-                VisibleCondition = sender => _active,
+                Scale = new Vector2(Program.Instance().BarScale,Program.Instance().BarScale),
+                VisibleCondition = sender => _active || Environment.TickCount - lastChange < 3000,
                 PositionUpdate =
                     () =>
                         new Vector2(Program.Instance().X, Program.Instance().Y - (_index * TextFont.Description.Height))
@@ -161,11 +181,11 @@ namespace UniversalRecallTracker
             Render.Text heroText = new Render.Text(0, 0, hero.ChampionName, TextFont.Description.Height, Color.White)
             {
                 OutLined = true,
-                VisibleCondition = sender => _active,
+                VisibleCondition = sender => _active || Environment.TickCount - lastChange < 3000,
                 PositionUpdate = delegate
                 {
-                    Rectangle rect = TextFont.MeasureText(null, hero.ChampionName, FontDrawFlags.Center);
-                    return new Vector2(_sprite.X - rect.Width - GapTextBar, _sprite.Y - (rect.Height / 2));
+                    Rectangle rect = TextFont.MeasureText(null, hero.ChampionName, 0);
+                    return new Vector2(_sprite.X - rect.Width - GapTextBar, _sprite.Y - rect.Height / 2 + (_sprite.Height * Program.Instance().BarScale)/2);
                 }
             };
 
@@ -215,17 +235,31 @@ namespace UniversalRecallTracker
             if (_active && _duration > 0)
             {
                 float percentage = (Game.ClockTime - _begin) / (_duration / 1000f);
-                int width =
-                    (int) (Properties.Resources.RecallBar.Width - (percentage * Properties.Resources.RecallBar.Width));
-                _sprite.Crop(0, 0, width, Properties.Resources.RecallBar.Height);
-                _countdownText.X = _sprite.X + width + GapTextBar;
+                int width = (int)(_sprite.Width - (percentage * _sprite.Width));
+                _countdownText.X = (int) (_sprite.X + (width * _sprite.Scale.X) + GapTextBar);
                 _countdownText.text =
                     Math.Round(
                         (Decimal) ((_duration / 1000f) - (Game.ClockTime - _begin)), 1, MidpointRounding.AwayFromZero) +
                     "s";
                 Rectangle rect = TextFont.MeasureText(null, _countdownText.text, FontDrawFlags.Center);
-                _countdownText.Y = _sprite.Y - (rect.Height / 2);
+                _countdownText.Y = (int) (_sprite.Y - rect.Height / 2 + (_sprite.Height * Program.Instance().BarScale) / 2);
+                _sprite.Crop(0, 0, width, _sprite.Height);
             }
+            else
+            {
+                _sprite.Crop(0, 0, _sprite.Width, _sprite.Height);
+            }
+        }
+
+        public void Reset()
+        {
+            lastChange = Environment.TickCount;
+        }
+
+        public void Scale(float barScale)
+        {
+            _sprite.Scale = new Vector2(barScale, barScale);
+            Reset();
         }
     }
 }
