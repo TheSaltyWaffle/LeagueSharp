0d0ac0644147dada33be7be0594b606e88c74129
TheSaltyWaffle <TheSaltyWaffle@users.noreply.github.com>
2016-03-23 21:19:27 +0100

    6.6 aurelian sol

--- UniversalMinimapHack/Properties/Resources.Designer.cs
+++ UniversalMinimapHack/Properties/Resources.Designer.cs
@@ -143,6 +143,16 @@ namespace UniversalMinimapHack.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap AurelionSol_Square_0 {
+            get {
+                object obj = ResourceManager.GetObject("AurelionSol_Square_0", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap Azir_Square_0 {
             get {
                 object obj = ResourceManager.GetObject("Azir_Square_0", resourceCulture);
