0ed7274eb3022eb4f09fa5220c5c2b8427f936d3
TheSaltyWaffle <TheSaltyWaffle@users.noreply.github.com>
2016-08-21 20:12:56 +0200

    Patch 6.16 icons

--- UniversalMinimapHack/Properties/Resources.Designer.cs
+++ UniversalMinimapHack/Properties/Resources.Designer.cs
@@ -613,6 +613,16 @@ namespace UniversalMinimapHack.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap Kled_Square_0 {
+            get {
+                object obj = ResourceManager.GetObject("Kled_Square_0", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap KogMaw_Square_0 {
             get {
                 object obj = ResourceManager.GetObject("KogMaw_Square_0", resourceCulture);
